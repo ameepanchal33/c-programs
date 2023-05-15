a295af883da24529c8ff36455844550576e2f560
Ami Panchal <amipanchal33@gmail.com>
2023-05-15 19:46:39 +0530

    sjavapr

---
+++ multi.java
@@ -0,0 +1,12 @@
+public class multi{
+       public static void main(String []args)
+{
+  int no=Integer.parseInt(args[0]);
+  for(int i=1; i<=10;i++)
+{
+  System.out.println(no+"*"+i+"="+(no*i));
+}
+
+}
+
+}
---
+++ pr.java
@@ -0,0 +1,12 @@
+public class pr{
+ public static void main(String args[]){
+ int no = Integer.parseInt(args[0]);
+int sum=0;
+ for(int i=0; i<=no; i++)
+{
+  sum+=i;
+  
+}
+System.out.println("sum is"+sum);
+}
+}
