3dfeff639d7ec7dfb20acdc02b496f63ea7cff18
Ami Panchal <amipanchal33@gmail.com>
2023-05-24 22:11:38 +0530

    primeno

---
+++ primeno.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class primeno{
+   public static void main (String args[])
+   {
+     int i,n;
+     for(i=2;i<=n-1;i++){
+      if(no%i==0){
+    temp=temp+1;   
+   }
+}
+if(temp==0)
+{
+  System.out.println("is prime");
+
+}
+else
+{
+  System.out.println("is not prime");
+}
+}
+
+
+
