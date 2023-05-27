f9e27ef007459e1f25ac2a5ec30e5432c1f124e0
Ami Panchal <amipanchal33@gmail.com>
2023-05-27 20:18:13 +0530

    fibonaci series

--- primeno.java
+++ primeno.java
@@ -1,23 +1,19 @@
-import java.util.Scanner;
-class primeno{
-   public static void main (String args[])
-   {
-     int i,n;
-     for(i=2;i<=n-1;i++){
-      if(no%i==0){
-    temp=temp+1;   
-   }
-}
-if(temp==0)
-{
-  System.out.println("is prime");
-
-}
-else
-{
-  System.out.println("is not prime");
-}
-}
-
-
-
+public class Primeno{    
+ public static void main(String args[]){    
+  int i,m=0,flag=0;      
+  int n=3;//it is the number to be checked    
+  m=n/2;      
+  if(n==0||n==1){  
+   System.out.println(n+" is not prime number");      
+  }else{  
+   for(i=2;i<=m;i++){      
+    if(n%i==0){      
+     System.out.println(n+" is not prime number");      
+     flag=1;      
+     break;      
+    }      
+   }      
+   if(flag==0)  { System.out.println(n+" is prime number"); }  
+  }//end of else  
+}    
+}   
